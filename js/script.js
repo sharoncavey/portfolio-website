e8d8495dd7665b3bdb5672e5d8328ca5e38976f6
sharoncavey <76669535+sharoncavey@users.noreply.github.com>
2021-01-28 17:47:23 +1100

    Updated javascript
    Fixed navigation menu error

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function menuToggle() {
-  var x = document.getElementByID('myNavtoggle');
+  var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
   } else {
